d910f714c637dc24d8ddd8b34954eeab6a050de2
Adam Struve <adam@adamstruve.com>
2023-03-25 23:05:04 -0400

    Initial commit

---
+++ better-thumbs.py
@@ -0,0 +1,47 @@
+# working for for SF gallery thumbnails. 
+import cv2
+import face_recognition
+import numpy as np
+from PIL import Image
+
+
+def create_thumbnail(input_image_path, output_image_path, thumbnail_size=(500, 280)):
+    image = cv2.imread(input_image_path)
+    height, width, _ = image.shape
+    aspect_ratio = float(width) / float(height)
+
+    target_width, target_height = thumbnail_size
+    target_aspect_ratio = float(target_width) / float(target_height)
+
+    if target_aspect_ratio > aspect_ratio:
+        new_width = target_width
+        new_height = int(new_width / aspect_ratio)
+    else:
+        new_height = target_height
+        new_width = int(new_height * aspect_ratio)
+
+    image_resized = cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_AREA)
+
+    face_locations = face_recognition.face_locations(image_resized)
+
+    if face_locations:
+        top, right, bottom, left = face_locations[0]
+
+        face_center_x = (right + left) // 2
+        face_center_y = (top + bottom) // 2
+
+        thumbnail_left = min(max(face_center_x - target_width // 2, 0), new_width - target_width)
+        thumbnail_top = min(max(face_center_y - target_height // 2, 0), new_height - target_height)
+    else:
+        thumbnail_left = (new_width - target_width) // 2
+        thumbnail_top = (new_height - target_height) // 2
+
+    thumbnail = image_resized[thumbnail_top:thumbnail_top + target_height, thumbnail_left:thumbnail_left + target_width]
+    cv2.imwrite(output_image_path, thumbnail)
+
+
+if __name__ == "__main__":
+    input_image_path = "1.jpg"
+    output_image_path = "1a.jpg"
+
+    create_thumbnail(input_image_path, output_image_path)
